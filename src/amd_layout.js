dc7e3b187c4de973d488d0b1e40089d3802872a4
Fluffy Heinzelman <micha.meyer@im-c.de>
2017-01-24 15:31:32 +0100

    Changed amd_layout.js to support CommonJs

--- src/amd_layout.js
+++ src/amd_layout.js
@@ -1,9 +1,19 @@
 (function(global, factory) {
-  if (typeof define === 'function' && define.amd)
-    define(function() { return factory(global) })
-  else
-    factory(global)
-}(this, function(window) {
-  YIELD
-  return Zepto
-}))
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        module.exports = global.document ? factory(global, true) : function(w) {
+            if (!w.document) {
+                throw new Error('Zepto requires a window with a document');
+            }
+            return factory(w);
+        }
+    }
+    else if (typeof define === 'function' && define.amd) {
+        define(function() { return factory(global) });
+    }
+    else {
+        factory(global);
+    }
+}(typeof window !== 'undefined' ? window : this, function(window) {
+    YIELD
+    return Zepto;
+}));
